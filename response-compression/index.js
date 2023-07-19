dd9b2a851f8aba814a4421459b9ba8289ff9394a
sjmalik <surajit.malik@qlikchain.com>
2023-07-19 08:51:57 +0530

    response compression update

--- response-compression/index.js
+++ response-compression/index.js
@@ -3,20 +3,27 @@ const compression = require('compression');
 
 const app = express();
 
-app.use(compression({
+// app.use(
+const compressMiddleware = compression({
     level: 6,   // value 6 is the best compression level to optimize the cpu
     threshold: 100 * 1024,
-    filter: (req, res)=> {
-        if(req.headers['x-no-compression']){
+    filter: (req, res) => {
+        if (req.headers['x-no-compression']) {
             return false;
         }
         return compression.filter(req, res)
     }
-}))
+})
+// )
 
-app.get('/', (req, res)=> {
+app.get('/without-compression', (req, res) => {
     const payload = 'Faster app which uses less bandwidth ...';
     res.send(payload.repeat(10000))
 });
 
-app.listen(3000, ()=> console.log('🚀 running at port 3000'))
+app.get('/with-compression', compressMiddleware, (req, res) => {
+    const payload = 'Faster app which uses less bandwidth ...';
+    res.send(payload.repeat(10000))
+});
+
+app.listen(3000, () => console.log('🚀 running at port 3000'))
